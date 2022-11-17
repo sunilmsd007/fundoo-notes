50a7afcccf317ad35d3ed7f3e1dad9d2e569633d
sunilmsd007 <sunilmsd007@gmail.com>
2022-11-17 13:09:10 +0530

    Refactored: to archive and Unarchive the note

--- src/services/notes.service.js
+++ src/services/notes.service.js
@@ -2,8 +2,8 @@ import Notes from '../models/notes.model';
 
 //create notes
 export const createNotes = async (body) => {
-  const data = await Notes.create(body);
-  return data;
+    const data = await Notes.create(body);
+    return data;
 };
 
 //get all notes
@@ -21,49 +21,57 @@ export const getNotesById = async (id) => {
 //update note
 export const updateNotesById = async (_id, body) => {
     const data = await Notes.findOneAndUpdate(
-      {
-        _id
-      },
-      body,
-      {
-        new: true
-      }
+        {
+            _id
+        },
+        body,
+        {
+            new: true
+        }
     );
     return data;
-  };
+};
 
-  //delete note
+//delete note
 export const deleteNotesById = async (id) => {
     await Notes.findByIdAndDelete(id);
     return '';
-  };
+};
 
-  //Archive note
-export const archiveNote = async (_id, body) => {
-    var body = {isArchive:true};
-    const data = await Notes.findByIdAndUpdate(
-      {
-        _id
-      },
-      body,
-      {
-        new: true
-      }
+//Archive note
+export const archiveNote = async (_id) => {
+    const noteData = await Notes.findById(_id);
+    let checkStatus = () => {
+        if (noteData.isArchive == false) {
+            return true
+        } else {
+            return false
+        }
+    }
+    let status = checkStatus();
+    const data1 = await Notes.findByIdAndUpdate(
+        {
+            _id
+        },
+        {isArchive:status},
+        {
+            new: true
+        }
     );
-    return data;
-  };
+    return data1;
+};
 
 //trash a note
 export const trashNote = async (_id, body) => {
-    var body = {isTrash:true};
+    body = { isTrash: true };
     const data = await Notes.findByIdAndUpdate(
-      {
-        _id
-      },
-      body,
-      {
-        new: true
-      }
+        {
+            _id
+        },
+        body,
+        {
+            new: true
+        }
     );
     return data;
-  };
+};
